3ef6b28da9cef02e2485f40971f1408f5d059f74
mordim3r <valent1n.bazylev@gmai.com>
2026-09-25 13:51:17 +0300

    add: create, update, find, delete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,22 @@
 import org.hibernate.SessionFactory;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        User user = new User("Andrew", "andrew@gmail.com", 22);
+       // User user = new User("Andrew", "andrew2@gmail.com", 22);
         UserStorage userStorage = new UserStorage();
-        userStorage.saveUser(user);
-        System.out.println(user);
+      //  userStorage.saveUser(user);
+      //  System.out.println(user);
+       // System.out.println("----find----");
+       // System.out.println(userStorage.findById(user.getId()));
+
+        List<User> users = userStorage.findAll();
+        users.forEach(System.out::println);
+        User user = userStorage.findById(3);
+        user.setName("Alex");
+        userStorage.updateUser(user);
+        System.out.println(userStorage.findById(3));
         HibernateUtil.shutdown();
     }
 }
--- src/main/java/UserStorage.java
+++ src/main/java/UserStorage.java
@@ -2,9 +2,7 @@
 import org.hibernate.SessionFactory;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-
-
-
+import org.hibernate.query.SelectionQuery;
 
 import java.util.List;
 
@@ -32,21 +30,62 @@ public class UserStorage implements UserRepository {
 
     @Override
     public User findById(int id) {
-        return null;
+        try (Session session = factory.openSession()){
+            return session.find(User.class, id);
+
+        }
+
     }
 
     @Override
     public List<User> findAll() {
-        return List.of();
+        try ( Session session = factory.openSession()){
+            String hql = "from User";
+            SelectionQuery <User> query = session.createSelectionQuery(hql,User.class);
+            return query.getResultList();
+        }
     }
 
     @Override
     public void updateUser(User user) {
+        if (user==null){
+            throw new IllegalArgumentException("User не может быть null");
+        }
+        if (user.getId()<=0){
+            throw new IllegalArgumentException("Для изменения записи нужен корректный id");
+        }
+        Transaction transaction =null;
+        try(Session session = factory.openSession()) {
+            transaction = session.beginTransaction();
+            User existingUser = session.find(User.class, user.getId());
+            if (existingUser==null){
+                transaction.rollback();
+                throw new IllegalArgumentException("Пользователь с таким id не найден");
+            }
+            existingUser.setName(user.getName());
+            existingUser.setEmail(user.getEmail());
+            existingUser.setAge(user.getAge());
+            transaction.commit();
+        } catch (RuntimeException e){
+            if (transaction != null && transaction.isActive()){
+                transaction.rollback();
+            }
+            throw e;
+        }
 
     }
 
     @Override
     public void deleteUser(int id) {
+        if (id <= 0) {
+            throw new IllegalArgumentException("Для удаления нужен корректный id");
+        }
+        try (Session session = factory.openSession()){
+            Transaction transaction = session.beginTransaction();
+            User existingUser = session.find(User.class, id);
+
+
+        }
 
     }
 }
